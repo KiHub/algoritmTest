89b3b0664b2290c8f3e8cddbd7d6f47381c8f59e
Kirill Ki <kirillkalapov@gmail.com>
2021-09-16 15:24:13 +0300

    Initial Commit

---
+++ algoritmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  algoritmTest
+//
+//  Created by  Mr.Ki on 16.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
